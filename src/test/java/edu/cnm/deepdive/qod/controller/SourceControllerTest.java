93c56bb9583e829238393827852526d3568c097e
Alana Chigbrow <alana.chigbrow@gmail.com>
2019-07-30 13:20:18 -0600

    Started source tests and docs.

---
+++ src/test/java/edu/cnm/deepdive/qod/controller/SourceControllerTest.java
@@ -0,0 +1,62 @@
+package edu.cnm.deepdive.qod.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.hasSize;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.cnm.deepdive.qod.QodApplicationTest;
+import java.util.Random;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.MethodMode;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.web.context.WebApplicationContext;
+
+@ExtendWith(RestDocumentationExtension.class)
+@SpringBootTest(classes = QodApplicationTest.class)
+public class SourceControllerTest extends BaseControllerTest {
+
+
+  @Autowired
+  protected SourceControllerTest(ObjectMapper mapper,
+      WebApplicationContext context, Random rng) {
+    super(mapper, context, rng);
+  }
+
+  @Test
+  @DirtiesContext(methodMode = MethodMode.AFTER_METHOD)
+  void postSource() throws Exception {
+    addSource("George Box")
+        .andExpect(status().isCreated())
+        .andExpect(jsonPath("$.id").exists());
+  }
+
+  @Test
+  @DirtiesContext(methodMode = MethodMode.AFTER_METHOD)
+  void getSources() throws Exception {
+    addSource("George Box");
+    addSource("Bill and Ted's Excellent Adventure");
+    getMockMvc().perform(
+        get("/sources")
+        .accept(MediaType.APPLICATION_JSON)
+        .with(oauthTokenRequired())
+    )
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$", hasSize(2)));
+  }
+
+  private ResultActions addSource(String name) throws Exception {
+    return getMockMvc().perform(post("/sources")
+        .contentType(MediaType.APPLICATION_JSON)
+        .accept(MediaType.APPLICATION_JSON)
+        .with(oauthTokenRequired())
+        .content(String.format("{\"name\": \"%s\"}", name)));
+  }
+}
